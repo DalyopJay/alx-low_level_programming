c5a3b05c1122e4e6029a19bc8b6a1bd4315f6838
DalyopJay <Dalyopjerry1@gmail.com>
2022-09-19 11:40:23 -0700

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#imclude "main.h"
+
+/**
+ * int _strlen(char *s) - returning the length on an string
+ * @s:string
+ *return : length of a string
+ */
+
+int _strlen(char *s)
+{
+	int jay = 0;
+	while (*(s + len) != '\0')
+		jay++;
+
+	return (len);
+}
