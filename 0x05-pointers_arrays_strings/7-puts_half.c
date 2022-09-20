6768daff01bf00f47dde7f32b283b9f299f2e614
DalyopJay <Dalyopjerry1@gmail.com>
2022-09-20 16:18:23 -0700

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,23 +5,19 @@
  *puts_half - prints a string
  * @str: string to print
  *
- * Return: nothin
+ * Return: nothing
 */
 
 void puts_half(char *str)
-
 {
-
-		int i, len = _strlen(str);
-
+	int i, len = _strlen(str);
 
 
-			for (i = ((len - 1) / 2) + 1; i < len; i++)
+	for (i = ((len - 1) / 2) + 1; i < len; i++)
 
-						putchar(*(str + i));
+		putchar(*(str + i));
 
-				putchar(10);
-
+ 	putchar(10);
 }
 
 /**
@@ -31,22 +27,15 @@ void puts_half(char *str)
  * Return: the length of the given string
 */
 
-
-
 int _strlen(char *s)
 
 {
+	int len = 0;
 
-		int len = 0;
+	while (*(s + len) != '\0')
 
+		len++;
 
-
-			while (*(s + len) != '\0')
-
-						len++;
-
-
-
-				return (len);
+	return (len);
 
 }
