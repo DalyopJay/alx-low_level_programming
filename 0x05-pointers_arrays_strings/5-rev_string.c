82b8b29587aa9989e5052d9f35ca6d654acc96c0
DalyopJay <Dalyopjerry1@gmail.com>
2022-09-20 16:48:04 -0700

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,11 +14,11 @@ void rev_string(char *s)
 	int i, tmp, len = _strlen(s);
 
 	for (i = 0; i < len / 2; i++)
-	    {	
-		tmp = *(s + i);
-		*(s + i) = *(s + len - i - 1);
-		*(s + len - i - 1) = tmp;
-	    }	
+		{	
+			tmp = *(s + i);
+			*(s + i) = *(s + len - i - 1);
+			*(s + len - i - 1) = tmp;
+		}	
 }
 /**
  *_strlen - returns the length of a string
