f97f19a10b373f907dc0d522287f15e3f75c07fc
DalyopJay <Dalyopjerry1@gmail.com>
2022-09-21 10:48:27 -0700

    0strcat

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,10 @@
+#define MAIN_H
+#include <stdio>
+/**
+ * main prototypes()
+ * int prototypes()
+ * char prototypes()
+*/
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -7,3 +14,6 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+
+
+#endif /"MAIN_H"/
